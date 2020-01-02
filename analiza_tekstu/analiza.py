eb9930243c4d1158330927a6bce0447dc5ddced4
ShiroTsuma-github <spam.shiro@gmail,com>
2020-01-02 23:20:06 +0100

    do_dodatkow

--- analiza_tekstu/analiza.py
+++ analiza_tekstu/analiza.py
@@ -1,24 +1,35 @@
 import os
-
+alphabet=('q','w','e','r','t','y','u','i','o','p','[',']','a','s','d','f','g','h','j',
+'k','l',';',':','{','}','"',"'",'|','z','x','c','v','b','n','m',',','<','.','>','?','/',
+'1','!','2','@','3','#','4','$','5','%','6','^','7','&','8','*','9','(',')','-','_','=','+',' ','~','0'
+)
+lacznie=0
 def sciezka(nazwa):
     script_dir = os.path.dirname(__file__)
     rel_path =nazwa
     abs_file_path = os.path.join(script_dir, rel_path)
     return abs_file_path
-
 def liczenie_char(text,char):
     count=0
     for i in text:
         if i==char:
             count+=1
     return count
+
+
 try:
     nazwa=input("Podaj nazwe pliku: ")
     file=open(sciezka(nazwa),"r")
     text=file.read()
-    print(liczenie_char(text,"a"))
-except FileNotFoundError:
-    pass
+    for i in range(0,len(alphabet)):
+        char=alphabet[i]
+        temp=liczenie_char(text,char)
+        lacznie=lacznie+temp
+        print("Dla znaku "+str(alphabet[i])+" ",temp)
+    print("Laczna ilosc: "+ str(lacznie))
+    #print(laczna_ilosc_char(text))
+except:
+    raise 
 
 finally:
     file.close()
