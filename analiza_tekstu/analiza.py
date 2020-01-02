b7708546f12657454b004cdabcc21c4e0425c015
ShiroTsuma-github <spam.shiro@gmail,com>
2020-01-02 22:13:49 +0100

    yup

--- analiza_tekstu/analiza.py
+++ analiza_tekstu/analiza.py
@@ -1,10 +1,8 @@
 import os
-def sciezka():
-    script_dir = os.path.dirname(__file__)
-    rel_path = "analiza_tekstu/Lorem_Ipsum.txt"
-    abs_file_path = os.path.join(script_dir, rel_path)
-    return abs_file_path
+script_dir = os.path.dirname(__file__)
+rel_path ="Lorem_Ipsum.txt"
+abs_file_path = os.path.join(script_dir, rel_path)
 
-file=open(abs_file_path,"r")
+file=open(str(abs_file_path),"r")
 print(file.read())
 file.close()
