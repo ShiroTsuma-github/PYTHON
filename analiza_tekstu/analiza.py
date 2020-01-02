eaec3e3f523bd11b8b3559ba715dfca9c5c06311
ShiroTsuma-github <spam.shiro@gmail,com>
2020-01-02 22:13:38 +0100

    yes

--- analiza_tekstu/analiza.py
+++ analiza_tekstu/analiza.py
@@ -1,9 +1,10 @@
-try:
-    f=open("Lorem_Ipsum.txt","r")
+import os
+def sciezka():
+    script_dir = os.path.dirname(__file__)
+    rel_path = "analiza_tekstu/Lorem_Ipsum.txt"
+    abs_file_path = os.path.join(script_dir, rel_path)
+    return abs_file_path
 
-except FileNotFoundError():
-    f=open(__file__,"r")
-    print("welp")
-finally:
-    f.close()
-    
+file=open(abs_file_path,"r")
+print(file.read())
+file.close()
