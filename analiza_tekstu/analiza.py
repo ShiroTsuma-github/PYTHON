12f38140385485b9d81098582b1f0e30820488db
ShiroTsuma-github <spam.shiro@gmail,com>
2020-01-02 22:34:41 +0100

    yes

--- analiza_tekstu/analiza.py
+++ analiza_tekstu/analiza.py
@@ -9,15 +9,16 @@ def sciezka(nazwa):
 def liczenie_char(text,char):
     count=0
     for i in text:
-        if c==char:
+        if i==char:
             count+=1
-            return count
+    return count
 try:
     nazwa=input("Podaj nazwe pliku: ")
     file=open(sciezka(nazwa),"r")
     text=file.read()
     print(liczenie_char(text,"a"))
-
+except FileNotFoundError:
+    pass
 
 finally:
     file.close()
