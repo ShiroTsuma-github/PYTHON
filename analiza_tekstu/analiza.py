8a9b0485f885ca863142cf562b84dd0003718fb1
ShiroTsuma-github <spam.shiro@gmail,com>
2020-01-03 14:17:53 +0100

    it's good?

--- analiza_tekstu/analiza.py
+++ analiza_tekstu/analiza.py
@@ -9,6 +9,7 @@ def sciezka(nazwa):
     sciezka_z_folderu_do_pliku =nazwa
     pelna_sciezka = os.path.join(sciezka_do_folderu, sciezka_z_folderu_do_pliku)
     return pelna_sciezka
+
 def liczenie_char(text,char):
     count=0
     for i in text:
@@ -16,18 +17,42 @@ def liczenie_char(text,char):
             count+=1
     return count
 
+def wyrazy(text):
+    wyrazy={}
+    temp=[]
+    count=0
+    for i in text:
+        if i != " ":          
+            temp.append(str(i))
+        else:
+            """
+            f=open("test.txt","a+")
+            f.write(str(temp))
+            f.close
+            """
+            wyraz=("".join(temp))
+            wyrazy[count]=wyraz #wystepuje problem,ze nie chce wpisywac,wiec robie droga na okolo
+            count+=1
+            temp.clear()
+    return wyrazy
+    
+    
+
 
 try:
     nazwa=input("Podaj nazwe pliku: ")
     file=open(sciezka(nazwa),"r")
     text=file.read()
+    
     for i in range(0,len(alphabet)):
         char=alphabet[i]
         temp=liczenie_char(text,char)
         lacznie=lacznie+temp
         print("Dla znaku "+str(alphabet[i])+" ",temp)
+    
     print("Laczna ilosc: "+ str(lacznie))
-    #print(laczna_ilosc_char(text))
+    
+    print(wyrazy(text))
 except:
     raise 
 
