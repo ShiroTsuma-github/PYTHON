5a1e901bb5fc8de02eceab6cfc483e1dea33135a
ShiroTsuma-github <spam.shiro@gmail,com>
2020-01-05 18:01:14 +0100

    end

--- testy/functional_programming/test1.py
+++ testy/functional_programming/test1.py
@@ -1,7 +1,31 @@
-def countdown():
-    i = 5
-    while i > 0:
-        yield i  # zwraca wartosc z funkcji generatora
-        i -= 1
-for i in countdown():
-    print(i)
+try:
+    def is_prime(num):
+        #tab=[x for x in range(1,num+1)]
+        count=0
+        for i in range(1,num+1):
+            if num%i==0:
+                count+=1
+            if count>2:
+                break
+        if count<=2:
+            return num
+        else:
+            pass
+                
+    
+    def get_primes():
+        try:
+            num = 1
+            while True:
+                if is_prime(num):      
+                    yield (num)
+                    num += 1
+                else:
+                    num+=1
+        except  GeneratorExit:
+            print("welp")
+
+    for i in get_primes():
+        print(i)
+except:
+    pass
