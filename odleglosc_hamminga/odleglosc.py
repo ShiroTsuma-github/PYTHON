b169a2faa9d9824a7fc79c287a3eb889c0c85019
ShiroTsuma-github <spam.shiro@gmail,com>
2020-03-27 13:10:03 +0100

    progress

--- odleglosc_hamminga/odleglosc.py
+++ odleglosc_hamminga/odleglosc.py
@@ -2,57 +2,72 @@ from colorama import init,deinit,reinit
 from colored import fg,bg,attr
 import threading
 import time
+import sys
 import os
 #========================================text color config=====================================
-reset=attr('reset')
-color_error=fg('#FF0000')
-color_success=fg('#00FF00')
-color_info=fg('#0000FF')
+def color(color,text):
+    get_color={
+    'error':'#FFOOOO',
+    'success':'#00FF00',
+    'process':'#FFFF00',
+    'info':'#0000FF',
+    }
+    reset=attr('reset')
+    return (fg(get_color[color])+text+reset)
 #=======================================input file config=======================================
 hand_input=False
-amount_of_files=1
+amount_of_files=2
+ten_folder = os.path.dirname(os.path.abspath(__file__))
+sciezki_plikow=[]
+nazwy_plikow=[]
 for i in range(0,amount_of_files):  
     if hand_input:
-        file_input=str(input(color_info+"Podaj nazwę pliku wraz z formatem (np. plik.txt ): "+ reset))
+        file_input=str(input(color('info',"Podaj nazwę pliku wraz z formatem (np. plik.txt ): ")))
     else:
-        file_input="lorem.txt"
-        print(color_info+f'Wprowadzanie ręczne wyłączone. Wybieranie domyślnego zdefiniowanego pliku: {file_input}' +reset)
+        file_input=f'lorem{i}.txt'
+        print(color('info',f'Wprowadzanie ręczne wyłączone. Wybieranie domyślnego zdefiniowanego pliku {i+1}: ')+file_input)
+    sciezki_plikow.append(os.path.join(ten_folder, file_input))
+    nazwy_plikow.append(file_input)
         
-THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-my_file = os.path.join(THIS_FOLDER, file_input)
+
 #========================================functions config========================================
 
 def count_lines(plik):
+    #LICZENIE LINIJEK UŻYTECZNE,JEŻELI PLANUJE NA KILKA THREADOW,ZEBY PODZIELIC KAZDEJ FRAGMENT PLIKU
     try:
         plik=open(plik,'r')
+        print(color('process',":::Liczenie linijek..."))
         num_lines = sum(1 for line in plik)
         plik_jest=True
     except:
         plik_jest=False
-        print(color_error+"Program nie może znaleźć pliku! Upewnij się,czy podałeś poprawną nazwę pliku i czy znajduje się w odpowiednim folderze.")
+        print(color('error',"Program nie może znaleźć pliku! Upewnij się,czy podałeś poprawną nazwę pliku i czy znajduje się w odpowiednim folderze."))
     finally:
         if plik_jest:
             plik.close()
-            print(color_success+f'Ilość linijek w pliku to {num_lines}'+reset)
+            #print(color_success+f'Ilość linijek w pliku to {num_lines}'+reset)
             return num_lines
-        print ("Brak pliku do odczytu. Przerywanie programu."+reset)
+        print (color('error',"Brak pliku do odczytu. Przerywanie programu."))
+        sys.exit()
 
-def format_to_compare(plik):
+def format_to_compare(plik,i):
     if count_lines(plik)>1: 
         try:
-            formated=f'formated_{file_input}'
-            write_file=open(f'{THIS_FOLDER}\{formated}','w')
-            for line in open(my_file,'r').readlines():
-                if line == '\n':
-                    pass
-                else:
-                    write_file.write()
-                    #write_file.write((line.replace(" ", "")))
+            formated=f'formated_{nazwy_plikow[i]}'
+            print(color('process',":::Formatowanie pliku..."))
+            write_file=open(f'{ten_folder}\{formated}','w')
+            for line in open(sciezki_plikow[i],'r').readlines():
+                write_file.write(line.replace("\n", ""))
+                #write_file.write((line.replace(" ", "")))
         except FileNotFoundError:
-            print(color_error+"Wystąpił błąd podczas próby formatowania tekstu."+reset)
+            print(color('error',"Wystąpił błąd podczas próby formatowania tekstu."))
         finally:
             write_file.close()
     else:
         pass
-format_to_compare(my_file)
+    
+    
+    
+for i in range(0,amount_of_files):
+    format_to_compare(sciezki_plikow[i],i)
 #count_lines(my_file)
