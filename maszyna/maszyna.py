bee1bbbcb60a926ae90e5a1dec1e3c9ae9af717b
ShiroTsuma-github <spam.shiro@gmail,com>
2020-03-31 11:42:24 +0200

    yes

--- maszyna/maszyna.py
+++ maszyna/maszyna.py
@@ -1,5 +1,68 @@
+import math
+import os
+class produkty():
+    def __init__(self,name,price):
+        self.name=name
+        self.price=price
+waluty=[500,200,100,50,20,10,5,2,1,0.5,0.2,0.1,0.05,0.01]
+p1=produkty("Coca-Cola",2.5)
+p2=produkty("Czipsy",1.8)
+p3=produkty("Ice-tea",2.3)
+p4=produkty("kawa",2)
+p5=produkty("woda",4.5)
+produkty=[p1,p2,p3,p4,p5]
+wplacone=0
+wplacone_m=[]
+temp=0
+
+ten_folder = os.path.dirname(os.path.abspath(__file__))
+ten_plik=(os.path.join(ten_folder, 'data.txt'))
+#=========================input=====================
+"""
+hand_input=False
+if hand_input:
+    end=False
+    while end != True:
+        t=input("Wsadź pieniądze:")
+        if t == 'ok':
+        wplacone.append(t)
+"""   
 try:
-    f=open('data.txt','r+')
+    f=open(ten_plik,'w+')
+    print("Wybierz produkt")
+    for i in produkty:
+        #print(i)
+        print(f'[{temp+1}]',i.name,' : ',i.price)
+        temp+=1
+    print("=============================")
+    x=int(input("Wybierz opcję: "))-1
+    temp=0
+    print("Obsługiwane waluty")
+    for i in waluty:
+        print(i,"zł")
+        """
+        if i>=1:
+            print(i ,"zł")
+        else:
+            print(int(i*100),"gr")
+        """
+        
+        
+    while (wplacone <produkty[x].price) :
+        temp=float(input((f"Wsadź pieniądze: {round(produkty[x].price-wplacone,2)} zł: ")))
+        if temp not in waluty:
+            print("Automat nie przyjmuje podanej waluty. Spróbuj podać po monecie")
+        else:
+            wplacone=temp+wplacone
+            wplacone_m.append(temp)
+    f.write(f'[{produkty[x].name},{produkty[x].price},{wplacone_m},{abs(round(produkty[x].price-wplacone,2))}]')
+    print(f'{produkty[x].name} zostanie zaraz wydany/a\n Pozostały balans:{abs(round(produkty[x].price-wplacone,2))}')
+    for i in range()
+
+    
+
+    
+    
 
 finally:
     f.close()
--- odleglosc_hamminga/odleglosc.py
+++ odleglosc_hamminga/odleglosc.py
@@ -38,36 +38,41 @@ for i in range(0,amount_of_files):
 
 #=========================================functions==============================================
 
-def count_lines(plik,i):
-    #LICZENIE LINIJEK UŻYTECZNE,JEŻELI PLANUJE NA KILKA THREADOW,ZEBY PODZIELIC KAZDEJ FRAGMENT PLIKU
-    try:
-        plik=open(plik,'r')
-        print(color('process',f':::Liczenie linijek pliku {nazwy_plikow[i]}...'))
-        num_lines = sum(1 for line in plik)
-        plik_jest=True
-    except:
-        plik_jest=False
-        print(color('error',f"Program nie może znaleźć pliku {nazwy_plikow[i]}! Upewnij się,czy podałeś poprawną nazwę pliku i czy znajduje się w folderze z programem."))
-    finally:
-        if plik_jest:
-            plik.close()
-            #print(color_success+f'Ilość linijek w pliku to {num_lines}'+reset)
-            ilosc_linijek.append(num_lines)
-            return num_lines
-        print (color('error',"Brak pliku do odczytu. Przerywanie programu."))
-        sys.exit()
+def count_lines():
+    fix_file()
+    for i in range(0,amount_of_files):
+        plik=formated_sciezki_plikow[i]
+        #LICZENIE LINIJEK UŻYTECZNE,JEŻELI PLANUJE NA KILKA THREADOW,ZEBY PODZIELIC KAZDEJ FRAGMENT PLIKU
+        try:
+            plik=open(plik,'r')
+            print(color('process',f':::Liczenie linijek pliku {nazwy_plikow[i]}...'))
+            num_lines = sum(1 for line in plik)
+            plik_jest=True
+        except:
+            plik_jest=False
+            print(color('error',f"Program nie może znaleźć pliku {nazwy_plikow[i]}! Upewnij się,czy podałeś poprawną nazwę pliku i czy znajduje się w folderze z programem."))
+        finally:
+            if plik_jest:
+                plik.close()
+                #print(color_success+f'Ilość linijek w pliku to {num_lines}'+reset)
+                ilosc_linijek.append(num_lines)
+            else:
+                print (color('error',"Brak pliku do odczytu. Przerywanie programu."))
+                sys.exit()
 
 def format_to_compare():
+    count_lines()
     for i in range(0,amount_of_files):
-        plik=sciezki_plikow[i]
-        if count_lines(plik,i)>1: 
+        plik=formated_sciezki_plikow[i]
+        if ilosc_linijek[i]>=1: 
             try:
-                formated=f'formated_{nazwy_plikow[i]}'
+                ###################################################
+                formated=f'formated_{nazwy_plikow[i]}_2'
                 formated_nazwy_plikow.append(formated)
                 print(color('process',f":::Formatowanie pliku {nazwy_plikow[i]}..."))
-                write_file=open(f'{ten_folder}\{formated}','w')
-                formated_sciezki_plikow.append(os.path.join(ten_folder,formated))
-                for line in open(sciezki_plikow[i],'r').readlines():
+                write_file=open(formated_sciezki_plikow[i],'w')
+                #formated_sciezki_plikow.append(os.path.join(ten_folder,formated))
+                for line in open(formated_sciezki_plikow[i],'r').readlines():
                     write_file.write(line.replace("\n", ""))
                     #write_file.write((line.replace(" ", "")))
             except FileNotFoundError:
@@ -77,9 +82,38 @@ def format_to_compare():
         else:
             return (color('error',"Podany plik jest pusty: ")+nazwy_plikow[i])
         
-def compare_sentence(formated_file):
-    pass
-    
-format_to_compare()
-compare_sentence("a")
+def compare_sentence():
+    for i in range(0,amount_of_files):
+        plik=formated_sciezki_plikow[i]
+        try:
+            plik_open=(open(plik,'r'))
+            for i in plik_open:
+                print(i)
+        finally:
+            plik_open.close() 
+
+def fix_file():
+    for i in range(0,amount_of_files):
+        plik=sciezki_plikow[i]
+        try:
+            formated=f'formated_{nazwy_plikow[i]}'
+            formated_nazwy_plikow.append(formated)
+            print(color('process',f":::Formatowanie pliku {nazwy_plikow[i]}..."))
+            write_file=open(f'{ten_folder}\{formated}','w')
+            formated_sciezki_plikow.append(os.path.join(ten_folder,formated))
+            for word in open(sciezki_plikow[i],'r'):
+                in_line=0
+                for char in word:
+                    in_line+=1
+                    if in_line>=80:
+                        write_file.write("\n")
+                        in_line=0
+                    write_file.write(char)                
+        finally:
+            write_file.close()
+ 
+      
+#format_to_compare()
+#compare_sentence()
 #count_lines(my_file)
+format_to_compare()
