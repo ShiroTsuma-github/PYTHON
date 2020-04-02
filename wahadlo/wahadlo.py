6ea250d3ea461fdc5026f21a2a6f75e877d2a4d1
ShiroTsuma-github <spam.shiro@gmail,com>
2020-04-02 16:23:37 +0200

    maybe_needed

--- wahadlo/wahadlo.py
+++ wahadlo/wahadlo.py
@@ -1,23 +1,46 @@
+import math
 hand_input=False
+
 if hand_input:
     _min=input("min: ")
     _max=input("max: ")
     _step=input("step: ")
     _rep=input("repeats: ")
 else:
-    _min=1
-    _max=3
-    _step=1
-    _rep=3
-    
+    _min=0
+    _max=8
+    _step=5
+    _rep=2
+_prev=_min
+x=(_prev+(_max-_prev)*2)%_step
+"""
 for i in range(0,_rep):
     #r=_max*2-_min
-    r=((_max)-_min)*2+1
-    for j in range(_min,r,_step):
-        if float(j) <=round(r/2):
-            print(j)
+    r=(_min+(_max-_min)*2)
+    for j in range(_prev,(_min+(_max-_min)*2),_step):
+        if j <=_max:
+            print(j,end=" ")
         else:
-            print(r-j)
+            print(r-j+_min,end=" ")
+        if j == r-x:
+           #pass
+           _prev=abs((r-j+_min)-_step)
     if i == _rep-1:
-        print(_min)
-        
+        print(_prev)
+"""
+for i in range(0,_rep):
+    #r=_max*2-_min
+    r=(_prev+(_max-_prev)*2)
+    for j in range(_prev,(_prev+(_max-_prev)*2),_step):
+        if j <=_max:
+            print(j,end=" ")
+        else:
+            print(r-j+_prev,end=" ")
+        if j == r-x:
+           #pass
+           _prev=abs((r-j+_prev)-_step)
+           x=(_prev+(_max-_prev)*2)%_step
+    if i == _rep-1:
+        if _prev-_step==_min:
+            _prev=_min
+        print(_prev)
