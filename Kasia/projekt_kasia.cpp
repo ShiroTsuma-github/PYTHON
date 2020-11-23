92a0017370ad3821b4031d3eb53b5f98130e4550
ShiroTsuma-github <spam.shiro@gmail,com>
2020-11-23 13:32:24 +0100

    dodana funkcja czy_skonczyc

--- Kasia/projekt_kasia.cpp
+++ Kasia/projekt_kasia.cpp
@@ -11,7 +11,57 @@ int dlugosc_liczby(int k)
     } while (k);
     return ilosc_cyfr;
 }
-
+int suma_rzedu(const int tablica[],const int komorki_w_rzedzie)
+{
+    unsigned int suma_pozycji=0;
+    for(int i=0;i<komorki_w_rzedzie;i++)
+    {
+        suma_pozycji+=tablica[i];
+    }
+    return suma_pozycji;
+}
+int poprawna_pozycja(int aktualna_strona[],const int komorki_w_rzedzie)
+{
+    char pole;
+    unsigned int pozycja;
+    while(true)
+    {
+        if(suma_rzedu(aktualna_strona,komorki_w_rzedzie)==0)      {return -1;}   
+        cout<<"Podaj pole do wykonania ruchu [A-F]"<<endl;
+        cin>>pole;
+        cout<<endl;
+        switch(pole)
+        {
+            case 'A': case 'a':
+                pozycja=0;
+                break;
+            case 'B': case 'b':
+                pozycja=1;
+                break;
+            case 'C': case 'c':
+                pozycja=2;
+                break;
+            case 'D': case 'd':
+                pozycja=3;
+                break;
+            case 'E': case 'e':
+                pozycja=4;
+                break;
+            case 'F': case 'f':
+                pozycja=5;
+                break;      
+            default:
+                cout<<"Błąd: [Wartość poza zakresem]";
+                pozycja=10;        
+        }
+        if(pozycja>=0 and pozycja<6)    
+        {   
+            if(aktualna_strona[pozycja]>0)  {return pozycja;}    
+            
+            else{cout<<"Błąd: [Podane pole jest puste]";}
+        }
+    }
+}
 void rysuj_fragment(const int wys_komorki,const int szer_kom,int pozycja_y,const int liczba)
 {
     int ile_pominac=0;
@@ -36,6 +86,36 @@ void rysuj_fragment(const int wys_komorki,const int szer_kom,int pozycja_y,const
         }
         // jezeli jest pustym miejscem w srodku komorki,to wyswietla ' '
         else    {cout<<' ';}
+    }
+}
+int czy_skonczyc(
+    const int gorne_komorki[],
+    const int dolne_komorki[],
+    int baza_lewa,
+    int baza_prawa,
+    const char aktualny_gracz,
+    const int komorki_w_rzedzie)
+{
+    unsigned int suma_gora=suma_rzedu(gorne_komorki,komorki_w_rzedzie);
+    unsigned int suma_dol=suma_rzedu(dolne_komorki,komorki_w_rzedzie);
+
+    if(suma_gora==0 or suma_dol==0)
+    {
+        if((baza_lewa+suma_gora>baza_prawa+suma_dol)and aktualny_gracz=='L')
+        {
+            cout<<"Gratulacje wygranej dla Gracza grającego do Lewej Bazy";
+            return 1;
+        }
+        else if((baza_prawa+suma_dol>baza_lewa+suma_gora)and aktualny_gracz=='P')
+        {
+            cout<<"Gratulacje wygranej dla Gracza grającego do Prawej Bazy";
+            return 1;
+        }
+        else 
+        {
+            cout<<"Gratulacje remisu";
+            return 1;
+        }
     }
 }
 void wyswietl_plansze(
@@ -80,14 +160,8 @@ void wyswietl_plansze(
         }
         cout<<endl;
     }
+        cout<<"                  A           B           C           D           E           F"<<endl;
 }
-
-
-
-
-
-
-
 
 
 int main()
@@ -102,5 +176,34 @@ int main()
     int dolne_komorki[komorki_w_rzedzie]={6,6,6,6,6,6};
     int baza_lewa=0;
     int baza_prawa=0;
+    unsigned int Tura=0;
+    const char Gracz_pierwszy='P';    //tu trzeba ze pierwszy gracz to dol i gra do prawej bazy
+    const char Gracz_drugi='L';
+    char Aktualny_gracz;
+    while(Tura>=0)
+    {
     wyswietl_plansze(gorne_komorki,dolne_komorki,baza_lewa,baza_prawa,wys_planszy,komorki_w_rzedzie,wys_komorki,szer_komorki,wys_bazy,szer_bazy);
+    // na poczatku podzielne,przez 2,wiec pierwszy zaczyna,potem w zaleznosci czy liczba podzielna,ten gracz
+    if(Tura%2==0)   {Aktualny_gracz=Gracz_pierwszy;}
+    // jezeli Tura jest nieparzysta 
+    else    {Aktualny_gracz=Gracz_drugi;}
+
+    if(Aktualny_gracz=='L')
+    {
+        int pole=poprawna_pozycja(gorne_komorki,komorki_w_rzedzie);
+        if(pole==-1)
+        {
+            
+        }
+    }
+    else
+    {
+        int pole=poprawna_pozycja(gorne_komorki,komorki_w_rzedzie);
+        cout<<pole;  
+    }
+    
+    
+    cin>>baza_prawa;
+    }
 }
+
